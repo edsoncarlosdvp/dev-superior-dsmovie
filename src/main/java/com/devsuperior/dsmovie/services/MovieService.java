a424abdc8c97e5b835ba37c5f409d0b9e5de8ee0
edsoncarlosdvp <edsoncarlos@ymail.com>
2022-01-14 08:36:26 -0300

    Buscas

--- src/main/java/com/devsuperior/dsmovie/services/MovieService.java
+++ src/main/java/com/devsuperior/dsmovie/services/MovieService.java
@@ -1,5 +1,32 @@
 package com.devsuperior.dsmovie.services;
 
-public class MovieService {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import com.devsuperior.dsmovie.dto.MovieDTO;
+import com.devsuperior.dsmovie.entities.Movie;
+import com.devsuperior.dsmovie.movie.repositories.MovieRepository;
+
+@Service
+public class MovieService {
+	
+	@Autowired
+	private MovieRepository repository;
+	
+	@Transactional(readOnly = true)
+	public Page<MovieDTO> findAll(Pageable pageable) {
+		Page<Movie> result = repository.findAll(pageable);
+		Page<MovieDTO> page = result.map(items -> new MovieDTO(items));
+		return page;
+	}
+	
+	@Transactional(readOnly = true)
+	public MovieDTO findById(Long id) {
+		Movie result = repository.findById(id).get();
+		MovieDTO dto = new MovieDTO(result);
+		return dto;
+	}
 }
